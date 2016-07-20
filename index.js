0976dc61107522f7a0a0a829bfd7666e2b3d777d
Kellye Greene <kellye@gust.com>
2016-07-20 17:42:10 -0400

    js arrays

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(kitten) {
+  kittens.push(kitten);
+  return kittens;
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(kitten) {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(kitten) {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(kitten) {
+  return [...kittens, kitten];
+}
+
+function prependKitten(kitten) {
+  return [kitten, ...kittens];
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, -1);
+}
+
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
